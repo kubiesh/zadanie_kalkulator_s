5930f1727c9b18a06d53c25e25757dbbd4d5e113
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-27 16:30:44 +0200

    Cleaning

--- src/main/java/com/kubiesh/zadanie_kalkulator_s/App.java
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/App.java
@@ -1,13 +1,20 @@
 package com.kubiesh.zadanie_kalkulator_s;
 
-/**
- * Hello world!
- *
- */
+import java.util.Map;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
     }
 }
