539668268eb6aa7e8d6819f527e44c0b1342204c
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-27 23:01:18 +0200

    Class initialization

---
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/service/LocalEarningsCalculator.java
@@ -0,0 +1,25 @@
+package com.kubiesh.zadanie_kalkulator_s.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+
+import com.kubiesh.zadanie_kalkulator_s.entities.Country;
+
+@PropertySource("classpath:LocalEarningsCalculator.properties")
+public class LocalEarningsCalculator {
+	
+	private Country countryData;
+	private BigDecimal dailyRate;
+	@Value("${dataFilePath}") 
+	private String dataFilePath;
+
+	public LocalEarningsCalculator(String currencyCode, BigDecimal dailyRate) {
+		
+	}
+	
+	private void fillCountryData(String currencyCode) {
+		
+	}
+}
