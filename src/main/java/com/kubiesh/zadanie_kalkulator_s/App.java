345704c6792bc9c334710395e511f2a0c02a9580
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-27 16:50:23 +0200

    Cosmetic changes

--- src/main/java/com/kubiesh/zadanie_kalkulator_s/App.java
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/App.java
@@ -1,13 +1,7 @@
 package com.kubiesh.zadanie_kalkulator_s;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class App 
