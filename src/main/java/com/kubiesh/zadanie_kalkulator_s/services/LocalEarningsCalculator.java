5161bce58f083c75f6e53492dd4a094f48482ef2
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-29 18:54:27 +0200

    Finished getLocalEarningsMethod

--- src/main/java/com/kubiesh/zadanie_kalkulator_s/services/LocalEarningsCalculator.java
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/services/LocalEarningsCalculator.java
@@ -1,13 +1,13 @@
 package com.kubiesh.zadanie_kalkulator_s.services;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
 
 import com.kubiesh.zadanie_kalkulator_s.entities.Country;
 
-
+@Service
 public class LocalEarningsCalculator {
 	
 	private Country countryData;
@@ -21,10 +21,13 @@ public class LocalEarningsCalculator {
 	}
 	
 	public BigDecimal getLocalEarnings() {
+		/* (dailyRate*workDays)*(1-taxRate)-fixedCosts */
+		BigDecimal localEarnings = dailyRate.multiply(new BigDecimal(workDays));
+		BigDecimal netPercentage = new BigDecimal(1).subtract(countryData.getTaxRate());
+		localEarnings=localEarnings.multiply(netPercentage);
+		localEarnings=localEarnings.subtract(countryData.getFixedCosts());
 		
-		BigDecimal localEarnings = dailyRate;
-		
-		
-		//TO DO: whole function after finished CountryDAO
+		localEarnings=localEarnings.setScale(2,RoundingMode.HALF_EVEN); //i.e. 10.50 instead of 10.5000000011
+		return localEarnings;
 	}
 }
