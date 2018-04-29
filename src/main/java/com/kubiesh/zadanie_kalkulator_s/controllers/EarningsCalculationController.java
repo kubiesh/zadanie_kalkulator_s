d98fbc31bbafbe41c4bedd0895cd699170cfc38c
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-29 19:44:50 +0200

    controller implementation

--- src/main/java/com/kubiesh/zadanie_kalkulator_s/controllers/EarningsCalculationController.java
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/controllers/EarningsCalculationController.java
@@ -2,14 +2,29 @@ package com.kubiesh.zadanie_kalkulator_s.controllers;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kubiesh.zadanie_kalkulator_s.dao.CountryDAO_json;
+import com.kubiesh.zadanie_kalkulator_s.dao.ICountryDAO;
+import com.kubiesh.zadanie_kalkulator_s.entities.Country;
+import com.kubiesh.zadanie_kalkulator_s.services.FinalEarningsCalculator;
+import com.kubiesh.zadanie_kalkulator_s.services.LocalEarningsCalculator;
+
 @RestController
 public class EarningsCalculationController {
+	
+	@Autowired
+	private ICountryDAO countryDAO;
+	@Autowired
+	private LocalEarningsCalculator localEarningsCalculator;
+	@Autowired
+	private FinalEarningsCalculator finalEarningsCalculator;
 	
 	@GetMapping("/{countryID}/{dailyRate}")
 	@CrossOrigin(origins = "http://localhost:4200")
@@ -20,8 +35,18 @@ public class EarningsCalculationController {
 	) 
 	{
 		
+		Country selectedCountry = null;
+		try {
+		selectedCountry = countryDAO.getCountry(countryID);
+		}
+		catch (Exception e) {
+			return new ResponseEntity<BigDecimal>(new BigDecimal(0),HttpStatus.BAD_REQUEST);
+		}
 		
-				return null;
+		BigDecimal localEarnings = localEarningsCalculator.getLocalEarnings(selectedCountry,dailyRate,countryDAO.getWorkDays());
+		BigDecimal finalEarnings = finalEarningsCalculator.getFinalEarnings(selectedCountry,localEarnings);
+		
+		return new ResponseEntity<BigDecimal>(finalEarnings,HttpStatus.OK);
 	}
 	
 }
