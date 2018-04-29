e0c2cc0335ff66961b46d6258a789d495dbc72f6
kubiesh <jakub.z.dabrowski@student.put.poznan.pl>
2018-04-29 17:58:12 +0200

    countryDAO with main methods

--- src/main/java/com/kubiesh/zadanie_kalkulator_s/dao/CountryDAO_json.java
+++ src/main/java/com/kubiesh/zadanie_kalkulator_s/dao/CountryDAO_json.java
@@ -1,11 +1,18 @@
 package com.kubiesh.zadanie_kalkulator_s.dao;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kubiesh.zadanie_kalkulator_s.entities.Country;
 
 @Component
@@ -14,22 +21,74 @@ public class CountryDAO_json implements ICountryDAO {
 	
 	@Value("${dataFilePath}") 
 	private String dataFilePath;
+	private JsonNode rootNode;
+	
 	private int workDays;
 	private Vector<Country> countries = new Vector<Country>();
 	
 	public CountryDAO_json() {
-		
+		readTreeFromFile();
+		loadWorkDays();
+		loadCountries();
 	};
 
 	@Override
 	public Country getCountry(String countryID) {
-		// TODO Auto-generated method stub
-		return null;
+		Iterator<Country> it = countries.iterator();
+		while (it.hasNext()) {
+			Country country=it.next();
+			if (countryID==country.getCountryID()) {
+				return country;
+			}
+		}
+		return null; //Never return null
 	}
 
 	@Override
 	public int getWorkDays() {
-		return workdays;
+		return workDays;
 	}
-
+	
+	private void readTreeFromFile() {
+		byte[] jsonData=null;
+		
+		try {
+			jsonData = Files.readAllBytes(Paths.get(dataFilePath));
+		} catch (IOException e) {
+			System.out.println("Blad odczytu z pliku pliku CountryDAO_json.properties");
+			return;
+		}
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		try {
+			rootNode = objectMapper.readTree(jsonData);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	private void loadWorkDays() {
+		this.workDays=rootNode.path("workDays").asInt();
+	}
+	private void loadCountries() {
+		/* Worst possible implementation */
+		JsonNode countriesNode = rootNode.path("countries");
+		Iterator<JsonNode> it=countriesNode.elements();
+		
+		while(it.hasNext()){
+			
+			JsonNode element = it.next();
+			Country country=new Country();
+			country.setCountryID(element.path("countryID").asText());
+			country.setCountryName(element.path("countryName").asText());
+			country.setCurrencyCode(element.path("currencyCode").asText());;
+			country.setTaxRate(new BigDecimal(element.path("taxRate").asText()));
+			country.setFixedCosts(new BigDecimal(element.path("taxRate").asText()));
+			
+			countries.addElement(country);
+		}
+	}
+	
 }
